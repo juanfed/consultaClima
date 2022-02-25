7295782e3f42c9e45efdf5cda061ceac89a14dab
juanfed <juanfed12345@gmail.com>
2022-02-24 21:10:03 -0500

    cambié el http a https para evitar problemas a la hora de estar publicado en github pages me genere un error por tratar de consumir un recurso de una ruta no segura

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
     const consultarAPI = async () => {
       if (consultar) {
         const appId = 'fafe14e9dc78b3b170440e07886a4358';
-        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
 
         const respuesta = await fetch(url);
         const resultado = await respuesta.json();
